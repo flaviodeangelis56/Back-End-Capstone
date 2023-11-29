3e97e69138fc6888a5cade0df6cf6e16fd8654c7
flaviodeangelis56 <deangelisflavio@gmail.com>
2023-11-29 14:54:15 +0100

    create-reservation-vehicles-inDB

--- src/main/java/flaviodeangelis/noleggioAuto/entities/Reservations.java
+++ src/main/java/flaviodeangelis/noleggioAuto/entities/Reservations.java
@@ -1,4 +1,29 @@
 package flaviodeangelis.noleggioAuto.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "reservations")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Reservations {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private LocalDate dataInizioPrestito;
+    private LocalDate dataRestituzionePrevista;
+    private LocalDate dataRestituzione;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    @OneToOne
+    @JoinColumn(name = "vehicle_id", referencedColumnName = "id")
+    private Vehicles vehicles;
 }
--- src/main/java/flaviodeangelis/noleggioAuto/entities/Vehicles.java
+++ src/main/java/flaviodeangelis/noleggioAuto/entities/Vehicles.java
@@ -1,4 +1,31 @@
 package flaviodeangelis.noleggioAuto.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "vehicles")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Vehicles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String imgVehicles;
+    private int annoProduzione;
+    private int cilindrata;
+    private int maxPower;
+    private double lunghezza;
+    private double larghezza;
+    private double altezza;
+    private double passo;
+    private int massa;
+    private String ConsumiMisto;
+    @OneToOne
+    @JoinColumn(name = "reservation_id", referencedColumnName = "id")
+    private Reservations reservations;
 }
